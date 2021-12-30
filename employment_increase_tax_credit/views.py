f23fdcb472734206eb299541f7d4130f08c7c51a
karimoon <karimoons@hanmail.net>
2021-12-30 16:10:04 +0900

    상시근로자수 계산에 수도권과 지방 구분 추가

--- employment_increase_tax_credit/views.py
+++ employment_increase_tax_credit/views.py
@@ -72,43 +72,71 @@ def calculate_employee(excel_file, business_start_date):
             year_month_string = str(year) + '-' + str(month)
 
             # 중도퇴사자의 경우 매월 상시근로자수 계산 시 말일 퇴사자는 해당 월의 상시근로자수에 포함, 말일 전 퇴사자는 제외. (홈택스 상담사례 '고용 증대' 2019-03-05)
-            df.loc[(df['입사일자'] <= pd.Timestamp(date)) & ((df['퇴사일자'] >= pd.Timestamp(date)) | df['퇴사일자'].isnull()), year_month_string] = '일반'
+            df.loc[((df['입사일자'] <= pd.Timestamp(date)) & ((df['퇴사일자'] >= pd.Timestamp(date)) | df['퇴사일자'].isnull())) & (df['근무지'] == '수도권'), year_month_string] = '일반(수도권)'
+            df.loc[((df['입사일자'] <= pd.Timestamp(date)) & ((df['퇴사일자'] >= pd.Timestamp(date)) | df['퇴사일자'].isnull())) & (df['근무지'] == '지방'), year_month_string] = '일반(지방)'
             # 29세 이하. 단시간 근로자는 제외
-            df.loc[(df[year_month_string] == '일반') & (df['병역차감나이'] <= 29) & (df['월소정60시간이상근무단시간근로자'] == 'X'), year_month_string] = '청년등'
+            df.loc[(df[year_month_string] == '일반(수도권)') & (df['병역차감나이'] <= 29) & (df['월소정60시간이상근무단시간근로자'] == 'X'), year_month_string] = '청년등(수도권)'
+            df.loc[(df[year_month_string] == '일반(지방)') & (df['병역차감나이'] <= 29) & (df['월소정60시간이상근무단시간근로자'] == 'X'), year_month_string] = '청년등(지방)'
             # 장애인, 상이자
-            df.loc[(df[year_month_string] == '일반') & (df['장애인및상이자'] != 'X'), year_month_string] = '청년등'
+            df.loc[(df[year_month_string] == '일반(수도권)') & (df['장애인및상이자'] != 'X'), year_month_string] = '청년등(수도권)'
+            df.loc[(df[year_month_string] == '일반(지방)') & (df['장애인및상이자'] != 'X'), year_month_string] = '청년등(지방)'
             # 518민주화운동부상자, 고엽제후유의증환자 (2019년 1월 1일 이후 개시하는 과세연도 분부터 적용)
             if year >= 2019:
-                df.loc[(df[year_month_string] == '일반') & (df['518민주화운동부상자및고엽제후유의증환자'] != 'X'), year_month_string] = '청년등'
+                df.loc[(df[year_month_string] == '일반(수도권)') & (df['518민주화운동부상자및고엽제후유의증환자'] != 'X'), year_month_string] = '청년등(수도권)'
+                df.loc[(df[year_month_string] == '일반(지방)') & (df['518민주화운동부상자및고엽제후유의증환자'] != 'X'), year_month_string] = '청년등(지방)'
             # 근로계약 체결일 현재 연령이 60세 이상인 사람
             if year >= 2021:
-                df.loc[(df[year_month_string] == '일반') & (df['입사일나이'] >= 60), year_month_string] = '청년등'
+                df.loc[(df[year_month_string] == '일반(수도권)') & (df['입사일나이'] >= 60), year_month_string] = '청년등(수도권)'
+                df.loc[(df[year_month_string] == '일반(지방)') & (df['입사일나이'] >= 60), year_month_string] = '청년등(지방)'
             
-    df_summary = pd.DataFrame(index=['청년등근로자수합계', '청년등외근로자수합계', '총상시근로자수합계', '개월수', '청년등근로자수', '청년등외근로자수', '총상시근로자수'], columns=range(start_year, end_year + 1)).fillna(0)
+    df_summary = pd.DataFrame(index=['청년등근로자수합계(수도권)', '청년등근로자수합계(지방)', '청년등근로자수합계(전체)', '일반근로자수합계(수도권)', '일반근로자수합계(지방)', '일반근로자수합계(전체)', '총상시근로자수합계(수도권)', '총상시근로자수합계(지방)', '총상시근로자수합계(전체)', '개월수', '청년등근로자수(수도권)', '청년등근로자수(지방)', '청년등근로자수(전체)', '일반근로자수(수도권)', '일반근로자수(지방)', '일반근로자수(전체)', '총상시근로자수(수도권)', '총상시근로자수(지방)', '총상시근로자수(전체)'], columns=range(start_year, end_year + 1)).fillna(0)
 
     for index in df:
         if '-' not in index:
             continue
 
-        df.loc['청년등근로자수', index] = df.loc[(df[index] == '청년등') & (df['월소정60시간이상근무단시간근로자'] == 'X'), index].count()
-        df.loc['청년등근로자수', index] = df.loc['청년등근로자수', index] + df.loc[(df[index] == '청년등') & (df['월소정60시간이상근무단시간근로자'] == '지원X'), index].count() * 0.5
-        df.loc['청년등근로자수', index] = df.loc['청년등근로자수', index] + df.loc[(df[index] == '청년등') & (df['월소정60시간이상근무단시간근로자'] == '지원O'), index].count() * 0.75
+        df.loc['청년등근로자수(수도권)', index] = df.loc[(df[index] == '청년등(수도권)') & (df['월소정60시간이상근무단시간근로자'] == 'X'), index].count()
+        df.loc['청년등근로자수(지방)', index] = df.loc[(df[index] == '청년등(지방)') & (df['월소정60시간이상근무단시간근로자'] == 'X'), index].count()
+        # 15세 이상 29세 이하인 사람 중 단시간 근로자는 청년등에서 제외하나 근로계약 체결일 현재 연령이 60세 이상인 사람의 경우 단시간 근로자 제외 규정 없음. (조특법 제26조의7 제3항 3호)
+        df.loc['청년등근로자수(수도권)', index] = df.loc['청년등근로자수(수도권)', index] + df.loc[(df[index] == '청년등(수도권)') & (df['월소정60시간이상근무단시간근로자'] == '지원X'), index].count() * 0.5
+        df.loc['청년등근로자수(지방)', index] = df.loc['청년등근로자수(지방)', index] + df.loc[(df[index] == '청년등(지방)') & (df['월소정60시간이상근무단시간근로자'] == '지원X'), index].count() * 0.5
+        df.loc['청년등근로자수(수도권)', index] = df.loc['청년등근로자수(수도권)', index] + df.loc[(df[index] == '청년등(수도권)') & (df['월소정60시간이상근무단시간근로자'] == '지원O'), index].count() * 0.75
+        df.loc['청년등근로자수(지방)', index] = df.loc['청년등근로자수(지방)', index] + df.loc[(df[index] == '청년등(지방)') & (df['월소정60시간이상근무단시간근로자'] == '지원O'), index].count() * 0.75
+        df.loc['청년등근로자수(전체)', index] = df.loc['청년등근로자수(수도권)', index] + df.loc['청년등근로자수(지방)', index]
 
-        df.loc['청년등외근로자수', index] = df.loc[(df[index] == '일반') & (df['월소정60시간이상근무단시간근로자'] == 'X'), index].count()
-        df.loc['청년등외근로자수', index] = df.loc['청년등외근로자수', index] + df.loc[(df[index] == '일반') & (df['월소정60시간이상근무단시간근로자'] == '지원X'), index].count() * 0.5
-        df.loc['청년등외근로자수', index] = df.loc['청년등외근로자수', index] + df.loc[(df[index] == '일반') & (df['월소정60시간이상근무단시간근로자'] == '지원O'), index].count() * 0.75
+        df.loc['일반근로자수(수도권)', index] = df.loc[(df[index] == '일반(수도권)') & (df['월소정60시간이상근무단시간근로자'] == 'X'), index].count()
+        df.loc['일반근로자수(지방)', index] = df.loc[(df[index] == '일반(지방)') & (df['월소정60시간이상근무단시간근로자'] == 'X'), index].count()
+        df.loc['일반근로자수(수도권)', index] = df.loc['일반근로자수(수도권)', index] + df.loc[(df[index] == '일반(수도권)') & (df['월소정60시간이상근무단시간근로자'] == '지원X'), index].count() * 0.5
+        df.loc['일반근로자수(지방)', index] = df.loc['일반근로자수(지방)', index] + df.loc[(df[index] == '일반(지방)') & (df['월소정60시간이상근무단시간근로자'] == '지원X'), index].count() * 0.5
+        df.loc['일반근로자수(수도권)', index] = df.loc['일반근로자수(수도권)', index] + df.loc[(df[index] == '일반(수도권)') & (df['월소정60시간이상근무단시간근로자'] == '지원O'), index].count() * 0.75
+        df.loc['일반근로자수(지방)', index] = df.loc['일반근로자수(지방)', index] + df.loc[(df[index] == '일반(지방)') & (df['월소정60시간이상근무단시간근로자'] == '지원O'), index].count() * 0.75
+        df.loc['일반근로자수(전체)', index] = df.loc['일반근로자수(수도권)', index] + df.loc['일반근로자수(지방)', index]
 
-        df.loc['총상시근로자수', index] = df.loc['청년등근로자수', index] + df.loc['청년등외근로자수', index]
+        df.loc['총상시근로자수(수도권)', index] = df.loc['청년등근로자수(수도권)', index] + df.loc['일반근로자수(수도권)', index]
+        df.loc['총상시근로자수(지방)', index] = df.loc['청년등근로자수(지방)', index] + df.loc['일반근로자수(지방)', index]
+        df.loc['총상시근로자수(전체)', index] = df.loc['총상시근로자수(수도권)', index] + df.loc['총상시근로자수(지방)', index]
 
-        df_summary.loc['청년등근로자수합계', int(index[:4])] = df_summary.loc['청년등근로자수합계', int(index[:4])] + df.loc['청년등근로자수', index]
-        df_summary.loc['청년등외근로자수합계', int(index[:4])] = df_summary.loc['청년등외근로자수합계', int(index[:4])] + df.loc['청년등외근로자수', index]
-        df_summary.loc['총상시근로자수합계', int(index[:4])] = df_summary.loc['총상시근로자수합계', int(index[:4])] + df.loc['총상시근로자수', index]
+        df_summary.loc['청년등근로자수합계(수도권)', int(index[:4])] = df_summary.loc['청년등근로자수합계(수도권)', int(index[:4])] + df.loc['청년등근로자수(수도권)', index]
+        df_summary.loc['청년등근로자수합계(지방)', int(index[:4])] = df_summary.loc['청년등근로자수합계(지방)', int(index[:4])] + df.loc['청년등근로자수(지방)', index]
+        df_summary.loc['청년등근로자수합계(전체)', int(index[:4])] = df_summary.loc['청년등근로자수합계(전체)', int(index[:4])] + df.loc['청년등근로자수(전체)', index]
+        df_summary.loc['일반근로자수합계(수도권)', int(index[:4])] = df_summary.loc['일반근로자수합계(수도권)', int(index[:4])] + df.loc['일반근로자수(수도권)', index]
+        df_summary.loc['일반근로자수합계(지방)', int(index[:4])] = df_summary.loc['일반근로자수합계(지방)', int(index[:4])] + df.loc['일반근로자수(지방)', index]
+        df_summary.loc['일반근로자수합계(전체)', int(index[:4])] = df_summary.loc['일반근로자수합계(전체)', int(index[:4])] + df.loc['일반근로자수(전체)', index]
+        df_summary.loc['총상시근로자수합계(수도권)', int(index[:4])] = df_summary.loc['총상시근로자수합계(수도권)', int(index[:4])] + df.loc['총상시근로자수(수도권)', index]
+        df_summary.loc['총상시근로자수합계(지방)', int(index[:4])] = df_summary.loc['총상시근로자수합계(지방)', int(index[:4])] + df.loc['총상시근로자수(지방)', index]
+        df_summary.loc['총상시근로자수합계(전체)', int(index[:4])] = df_summary.loc['총상시근로자수합계(전체)', int(index[:4])] + df.loc['총상시근로자수(전체)', index]
         df_summary.loc['개월수', int(index[:4])] = df_summary.loc['개월수', int(index[:4])] + 1
 
     for index in df_summary:
-        df_summary.loc['청년등근로자수', index] = int((df_summary.loc['청년등근로자수합계', index] / df_summary.loc['개월수', index]) * 100) / 100
-        df_summary.loc['청년등외근로자수', index] = int((df_summary.loc['청년등외근로자수합계', index] / df_summary.loc['개월수', index]) * 100) / 100
-        df_summary.loc['총상시근로자수', index] = int((df_summary.loc['총상시근로자수합계', index] / df_summary.loc['개월수', index]) * 100) / 100
+        df_summary.loc['청년등근로자수(수도권)', index] = int((df_summary.loc['청년등근로자수합계(수도권)', index] / df_summary.loc['개월수', index]) * 100) / 100
+        df_summary.loc['청년등근로자수(지방)', index] = int((df_summary.loc['청년등근로자수합계(지방)', index] / df_summary.loc['개월수', index]) * 100) / 100
+        df_summary.loc['청년등근로자수(전체)', index] = df_summary.loc['청년등근로자수(수도권)', index] + df_summary.loc['청년등근로자수(지방)', index]
+        df_summary.loc['일반근로자수(수도권)', index] = int((df_summary.loc['일반근로자수합계(수도권)', index] / df_summary.loc['개월수', index]) * 100) / 100
+        df_summary.loc['일반근로자수(지방)', index] = int((df_summary.loc['일반근로자수합계(지방)', index] / df_summary.loc['개월수', index]) * 100) / 100
+        df_summary.loc['일반근로자수(전체)', index] = df_summary.loc['일반근로자수(수도권)', index] + df_summary.loc['일반근로자수(지방)', index]
+        df_summary.loc['총상시근로자수(수도권)', index] = int((df_summary.loc['총상시근로자수합계(수도권)', index] / df_summary.loc['개월수', index]) * 100) / 100
+        df_summary.loc['총상시근로자수(지방)', index] = int((df_summary.loc['총상시근로자수합계(지방)', index] / df_summary.loc['개월수', index]) * 100) / 100
+        df_summary.loc['총상시근로자수(전체)', index] = df_summary.loc['총상시근로자수(수도권)', index] + df_summary.loc['총상시근로자수(지방)', index]
 
     download_excel_file = IO()
     xlwriter = pd.ExcelWriter(download_excel_file, engine='xlsxwriter')
